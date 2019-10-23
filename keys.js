ff9571d5740742dd9be431a5a82117a8c3ab0e0e
Maverick <maverick.schirmer@gmail.com>
2019-10-22 18:26:19 -0700

    Updated read me and added node packages

--- keys.js
+++ keys.js
@@ -1,2 +1,15 @@
 console.log('this is loaded');
 
+//Spotify Node Package
+const Spotify = require('node-spotify-api')
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+const axios = require('axios')
+const db = require('db')
+const moment = require('moment')
+
+
+module.exports
