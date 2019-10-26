3dcdae5f2121ffc65c389c22def8aca6470412ca
Maverick <maverick.schirmer@gmail.com>
2019-10-25 17:15:46 -0700

    Grabbed specific data from APIs

--- keys.js
+++ keys.js
@@ -7,26 +7,9 @@ const spotify = new Spotify ({
   id:'e24c9bf78167492eaee3843fe4174bd9',
   secret: 'df57dded0909408a8384bf81c03ecb2e'
 })
-spotify
-  .search({ type: 'track', query: `beat it`, limit: 3 })
-  .then(({tracks}) => {
-    for (let i = 0; i < 3; i++) {
-      //Artists
-    console.log(tracks.items[i].artists[0].name);
-      //Name of the song
-    console.log(tracks.items[i].name)
-    // Preview Url
-    console.log(tracks.items[i].preview_url)
-    // Album Name
-    console.log(tracks.items[i].album.name)
-    }
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
 
 
-// const moment = require('moment')
+
 
 
 // module.exports
--- liri.js
+++ liri.js
@@ -1,44 +1,77 @@
 const fs = require('fs')
+const moment = require('moment')
 require("dotenv").config();
 const axios = require('axios')
-// const db = require('db')
 const keys = require("./keys.js");
+const spotify = new Spotify(keys.spotify);
+// const db = require('db')
 
 
+const now = moment()
 //Spotify
 // const spotify = new Spotify(keys.spotify)
 
 //Bands in Town
 
-// axios.get(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp&date=upcoming`)
-//   .then(data => {
-//     console.log(data)
-//   })
-//   .catch(e => console.log(e))
 
-//OMDB API
 
-axios.get(`http://www.omdbapi.com/?t=${input}&apikey=trilogy`)
-  .then(({data}) => {
-    console.log(data.Year)
-  })
-  .catch(e => console.log(e))
+
+
+
 
 // Switch statement to create the commands
-let command = process.argvb[2]
-const input = process.argv[3]
+let command = process.argv[2]
+let input = process.argv.slice(3).join(' ')
 
-// switch(command) {
-//   case `concert-this`:
-//     break;
+switch(command) {
+  case `concert-this`:
+      axios.get(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`)
+      .then(({data}) => {
+        for( let i = 0; i < 20; i++){
+        console.log('Venue Location: ' + data[i].venue.city +', ' + data[0].venue.country)
+        console.log('Venue: ' + data[i].venue.name)
+        console.log('Date: ' + moment(data[i].datetime).format('LLLL'))
+        }
+      })
+      .catch(e => console.log(e))
+    break;
   
-//   case `spotify-this-song`:
-//     break;
+  case `spotify-this-song`:
+      spotify
+      .search({ type: 'track', query: `${input}`, limit: 3 })
+      .then(({tracks}) => {
+        for (let i = 0; i < 3; i++) {
+          //Artists
+        console.log(tracks.items[i].artists[0].name);
+          //Name of the song
+        console.log(tracks.items[i].name)
+        // Preview Url
+        console.log(tracks.items[i].preview_url)
+        // Album Name
+        console.log(tracks.items[i].album.name)
+        }
+      })
+      .catch(function(err) {
+        console.log(err);
+      });
+    break;
 
-//   case `movie-this`:
-//     break;
+  case `movie-this`: 
+  axios.get(`http://www.omdbapi.com/?t=${input}&apikey=trilogy`)
+  .then(({data}) => {
+    console.log('Movie Title: ' + data.Title)
+    console.log('Year of Release: ' + data.Year)
+    console.log(`IMDB Rating: ${data.Ratings[0].Value}`)
+    console.log(`Rotten Tomatoes Rating: ${data.Ratings[1]}`)
+    console.log('Country where it was produced: ' + data.Country)
+    console.log('Language of the Movie: ' + data.Language)
+    console.log('Plot: ' + data.Plot)
+    console.log('Actors: ' + data.Actors)
+  })
+  .catch(e => console.log(e))
+    break;
 
-//   case `do-what-it-says`:
-//     break;
+  case `do-what-it-says`:
+    break;
 
-// }
+}
